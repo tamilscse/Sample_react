c60583acc78f3c578f345a0b1c0eb31375ca4c48
tamilscse <140077476+tamilscse@users.noreply.github.com>
2023-07-20 18:28:40 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  let name = ["tamil","AP"]; 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello {name[1]}!</h1>
+      <p>Start React :)</p>
     </div>
   );
 }
